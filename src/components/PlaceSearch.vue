f8e9da8359721a6685d062578157e6e839ec8ca9
RobertOrthofer <orthofer.robert@gmail.com>
2024-10-31 16:10:31 +0100

    implement help window

--- src/components/PlaceSearch.vue
+++ src/components/PlaceSearch.vue
@@ -9,6 +9,8 @@
     clearable
     density="compact"
     prepend-inner-icon="mdi-magnify"
+    prepend-icon="mdi-information"
+    @click:prepend="showInfo = !showInfo"
     :items="items"
     :loading="!!abortController"
     item-title="properties.name"
@@ -27,6 +29,38 @@
       ></v-list-item>
     </template>
   </v-autocomplete>
+  <v-expand-transition>
+    <v-card v-show="showInfo">
+      <v-card-title>
+        <v-icon size="small"> mdi-information-outline </v-icon>
+        Ortssuche
+      </v-card-title>
+      <v-card-subtitle>
+        Die Suche des
+        <a href="https://kataster.bev.gv.at">Österreichischen Katasters</a
+        ><br />
+        Suche nach Orten, Adressen, und mehr
+      </v-card-subtitle>
+      <v-card-text>
+        <v-timeline density="compact" align="start">
+          <v-timeline-item
+            v-for="(helpItem, key) in helpItems"
+            :key="key"
+            density="compact"
+            dot-color="success"
+            icon="mdi-text-search-variant"
+          >
+            <div>
+              <div class="font-weight-normal">
+                <strong> {{ helpItem.name }}</strong>
+              </div>
+              {{ helpItem.example }}
+            </div>
+          </v-timeline-item>
+        </v-timeline>
+      </v-card-text>
+    </v-card>
+  </v-expand-transition>
 </template>
 
 <script setup>
@@ -62,7 +96,31 @@ const { result } = usePlaceSearch();
 const emit = defineEmits(['result']);
 
 const model = ref(null);
+const showInfo = ref(false);
 const items = shallowRef([]);
+
+const helpItems = [
+  {
+    name: 'Orte, Dörfer, Gemeinden',
+    example: 'Salzburg, Liezen, Bad Aussee, Praunfalk',
+  },
+  {
+    name: 'Adressen, Straßen, Hausnummern',
+    example: 'Johannes Filzer Straße 5 5020 Salzburg',
+  },
+  {
+    name: 'KG-Nummer, Grundstücksnummer',
+    example: '49203-4',
+  },
+  {
+    name: 'Flurname, Riednamen, Einzelhäuser',
+    example: 'Ziegenreith, Unterstegalpe',
+  },
+  {
+    name: 'sonstige geographische Gebiete',
+    example: 'Wörthersee, Schwarze Sulm, Pötschenpass',
+  },
+];
 
 /** @type {import("vue").Ref<AbortController>} */
 const abortController = ref(null);
